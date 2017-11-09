cf326b80b781fc3b6d6ebb45d5a00677844be2cb
wt <wting1107@163.com>
2017-11-09 16:11:54 +0800

    登陆功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -46,17 +46,39 @@
              }
          }
      });
-     $form.on('susess.form.bv', function (e) {
+    //  表单校验成功,注册表单校验成功的事件，阻止默认，使用ajax提交
+     $form.on('success.form.bv', function (e) {
          /*禁用默认提交的事件,因为要使用ajax提交而不是默认的提交方式*/
          e.preventDefault();
          /*发送登陆的请求*/
          $.ajax({
              url: '/employee/employeeLogin',
              type: 'post',
-             dataType: 'json',
+             dataType: "json",
+             data:$form.serialize(),
              success: function (data) {
+                 console.log(data);
+                /* 跳转到首页 */  
+                 if(data.success){
+                     location.href = 'index.html';
+                 }
+                //  如果error是1000，提示用户名错误
+                if(data.erro == 1000){
+                    $form.data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
+                }
+                // error是1001，提示密码错误
+                if(data.erro == 1001){
+                    $form.data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
+                }
              }
 
          })
      })
+    //  重置功能
+    $("[type='reset']").on("click", function(){
+        
+        //重置表单样式
+        #form.data("bootstrapValidator").resetForm();
+        
+      });
  })
